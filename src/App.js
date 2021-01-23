3f9c7270f62d0ea70b54c1a6a85a3cb958832c45
ShibilPM <shibilpm3232@gmail.com>
2021-01-23 09:52:59 +0530

    Add collection preview

--- src/App.js
+++ src/App.js
@@ -3,20 +3,13 @@ import "./App.css";
 import { Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const Cats = () => {
-  return (
-    <div>
-      <h1>Cats</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Route path="/" exact component={HomePage} />
-      <Route path="/shop/cats" exact component={Cats} />
+      <Route path="/shop" exact component={ShopPage} />
     </div>
   );
 }
